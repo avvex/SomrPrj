dbd9fb6fe874d621bbb41f2a6c620a00abadffcf
Valentina Andreeva <50757293+avvex@users.noreply.github.com>
2022-02-24 11:15:47 +0700

    AddingFiles

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <QCoreApplication>
+class A
+{
+
+public:
+    int X;
+};
+
+class B
+{
+
+public:
+    int X;
+};
+
+class Steck
+{
+
+public:
+    int X;
+};
+
+int main(int argc, char *argv[])
+{
+    //QCoreApplication a(argc, argv);
+
+    return      1; //a.exec();
+}
